6f6d6136ed04f0922d25ca2e89b19af1843ebac6
Will <doggy.huang@gmail.com>
2015-11-08 15:58:48 +0800

    18 更新 Product 資料表的 ProductName 欄位型態為 nvarchar(80)，並設定 EDMX 從資料庫更新模型

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -11,31 +11,19 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
     
     public partial class Product
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Product()
         {
-            this.Stock = 1;
-            this.Active = true;
-
             this.OrderLine = new HashSet<OrderLine>();
         }
     
         public int ProductId { get; set; }
-
-        [Required]
-        [StringLength(100)]
         public string ProductName { get; set; }
-        [Required]
-        [Range(1.0, 99.9)]
-        [DisplayFormat(DataFormatString="{0:C}")]
         public Nullable<decimal> Price { get; set; }
-        [Required]
         public Nullable<bool> Active { get; set; }
-        [Required]
         public Nullable<decimal> Stock { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
