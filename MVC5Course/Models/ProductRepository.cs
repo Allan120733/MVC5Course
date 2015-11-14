cfbccc95ad32e7a52537d9f3d400e5451930cb75
Will <doggy.huang@gmail.com>
2015-11-14 10:39:52 +0800

    24 修改 ProductRepository 覆寫 All() 方法，加入 p.Active == true 的篩選條件

--- MVC5Course/Models/ProductRepository.cs
+++ MVC5Course/Models/ProductRepository.cs
@@ -6,6 +6,23 @@ namespace MVC5Course.Models
 {   
 	public  class ProductRepository : EFRepository<Product>, IProductRepository
 	{
+        public override IQueryable<Product> All()
+        {
+            return base.All().Where(p => p.Active == true);
+        }
+
+        public override IQueryable<Product> All(bool isGetAll)
+        {
+            if (isGetAll)
+            {
+                return base.All();
+            }
+            else
+            {
+                return this.All();
+            }
+        }
+
         public IQueryable<Product> Get取得前面10筆範例資料()
         {
             return this.All().Where(p => p.ProductId < 10);
