177cab1b37078ca325ab3b00eb8c8c13cae71820
Will <doggy.huang@gmail.com>
2015-11-28 17:44:47 +0800

    使用 Enum 輔助方法 & 編輯器/顯示範本

---
+++ MVC5Course/Models/IdeaVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public enum IdeaType
+    {
+        爛透了,
+        很棒,
+        普通
+    }
+    public class IdeaVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IdeaType IdeaType { get; set; }
+    }
+}
